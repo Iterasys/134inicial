621b3f1652065698a9266f732b6d3788b0525d89
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-05-05 22:12:25 -0300

    Criação de teste de unidade com leitura de uma lista de valores

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import somar, subtrair, multiplicar, dividir
 
 
@@ -70,6 +72,27 @@ def teste_dividir_negativo():
 
     # 2 - Executa
     resultado_obtido = dividir(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
+
+
+# lista para uso como massa de teste
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+]
+
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+    # 1 - Configura
+    # utilizamos a lista como massa de teste
+
+    # 2 - Executa
+    resultado_obtido = somar(numero_a, numero_b)
 
     # 3 - Valida
     assert resultado_obtido == resultado_esperado
