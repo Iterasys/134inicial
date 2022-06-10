a0dbc53a40f2abe07baf16401d4e75dd75bda530
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-06-09 22:10:09 -0300

    Enderecamento de arquivo compativel com diversos sistemas operacionais. Encadeamento de 2 chamadas de API

--- tests/api/test_store.py
+++ tests/api/test_store.py
@@ -1,10 +1,11 @@
-# ToDo: 3 - criar uma venda de um pet para um usuário
 # ToDo: 4 - consultar os dados do pet que foi vendido
 import json
+import os
+
 
 import requests
 
-url = 'https://petstore.swagger.io/v2/store/order'
+url = 'https://petstore.swagger.io/v2/'
 headers = {'Content-Type': 'application/json'}
 
 
@@ -20,10 +21,13 @@ def teste_vender_pet():
     status_pedido_esperado = 'placed'
 
     # Executa
+    caminho = os.path.abspath(__file__ + "/../../../") + os.sep + 'vendors' + os.sep + 'json' + os.sep
+    # print(f'caminho1 = {caminho}')
+
     resultado_obtido = requests.post(
-        url=url,
+        url=url + 'store/order',
         headers=headers,
-        data=open('C:\\Users\\corre\\PycharmProjects\\134inicial\\vendors\\json\\pedido1.json')
+        data=open(caminho + 'pedido1.json')
     )
 
     # Valida
@@ -35,3 +39,32 @@ def teste_vender_pet():
     assert corpo_do_resultado_obtido['id'] == pedido_id_esperado
     assert corpo_do_resultado_obtido['petId'] == pet_id_esperado
     assert corpo_do_resultado_obtido['status'] == status_pedido_esperado
+
+    # Extrai
+    pet_id_extraido = corpo_do_resultado_obtido.get('petId')
+
+    # Realizar a 2ª Transação
+
+    # Configura
+    # Dados de Entrada
+    # Extraidos da 1ª transação acima
+
+    # Resultado Esperado
+    pet_name_esperado = 'Snoopy'
+    status_code_esperado = 200
+
+    # Executa
+    resultado_obtido = requests.get(
+        url=url + 'pet/' + str(pet_id_extraido),
+        headers=headers
+    )
+
+    # Valida
+    assert resultado_obtido.status_code == status_code_esperado
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(json.dumps(corpo_do_resultado_obtido, indent=4))
+    assert corpo_do_resultado_obtido['name'] == pet_name_esperado
+
+
+
+
