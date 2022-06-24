4e5892d09527049d219db9437bd754f4315ffa48
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-06-23 22:05:17 -0300

    Automatizado o fluxo na pagina de Lista de produtos

--- tests/web/selenium_webdriver.py
+++ tests/web/selenium_webdriver.py
@@ -1,5 +1,6 @@
 # Configura
 # Biblioteca / Imports
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -26,6 +27,8 @@ class Testes:
         self.driver = webdriver.Chrome(
             'C:\\Users\\corre\\PycharmProjects\\134inicial\\vendors\\drivers\\chromedriver102.exe'
         )
+        # espera ate 15 segundo por qualquer elemento
+        # self.driver.implicitly_wait(15)
 
     # Fim
     def teardown_method(self):
@@ -43,18 +46,26 @@ class Testes:
         lista = self.driver.find_element(By.NAME, "fromPort")
         lista.click()
         # selecionar a cidade de origem desejada
-        lista.find_element(By.XPATH, '//option[ .= "São Paolo"]').click()
+        lista.find_element(By.XPATH, f'//option[ .= "{origem}"]').click()
+
         # clicar na lista de cidades de destino
         lista = self.driver.find_element(By.NAME, 'toPort')
         lista.click()
+
         # selecionar a cidade de destino desejada
-        lista.find_element(By.XPATH, '//option[ .= "New York"]').click()
+
+        lista.find_element(By.XPATH, f'//option[ .= "{destino}"]').click()
+
         # clicar no botão de procurar voos
-        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary')
+        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
+
 
         # Pagina Lista de Passagens
         # Executa / Valida
+        assert self.driver.find_element(By.TAG_NAME, 'h3').text == titulo_passagens_esperado
 
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click()
+        time.sleep(3)
         # Pagina de Compra
 
         # Executa / Valida
